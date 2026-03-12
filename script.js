f7539c121928381bf440a1aa98af5a4370022a88
antoniodroios <antonybey13@gmail.com>
2026-03-11 18:20:28 -0600

    feat: 3 tulips in canvas

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btn = document.getElementById('action-btn');
-    const tulip = document.querySelector('.tulip');
+    const plantContainer = document.querySelector('.plant-container');
     let isGrown = false;
 
     btn.addEventListener('click', () => {
         if (!isGrown) {
-
-            tulip.classList.add('growing');
+            plantContainer.classList.add('growing');
             btn.textContent = 'Observando el crecimiento...';
             btn.disabled = true;
 
+            // Esperar a que la flor del centro (la más lenta) termine (2.7s + 1.5s = 4.2s aprox)
             setTimeout(() => {
                 btn.disabled = false;
                 btn.textContent = 'Marchitar y reiniciar 🥀';
@@ -17,15 +17,16 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 4500);
 
         } else {
-            tulip.classList.remove('growing');
+            plantContainer.classList.remove('growing');
             btn.textContent = 'Preparando tierra...';
             btn.disabled = true;
 
+            // La reducción tarda aprox 1.5s
             setTimeout(() => {
                 btn.disabled = false;
                 btn.textContent = 'Plantar y Crecer 🌷';
                 isGrown = false;
-            }, 2500);
+            }, 2000); 
         }
     });
 });
